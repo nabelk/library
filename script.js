8a2ea76fdecf1be3f68472006c6c793b28ab6f3f
Nabil Khalid <holanabilkhalid@gmail.com>
2022-12-15 23:47:36 +0800

    Reorder and add remove button event

--- script.js
+++ script.js
@@ -7,29 +7,38 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+const bookCards = document.getElementById('book-cards');
+
 function addBookToLibrary(title, author, pages, read) {
     const addBook = new Book(title, author, pages, read);
     library.push(addBook);
-}
-
-addBookToLibrary('The hunger game', 'someone', 200, 'read');
-addBookToLibrary('Harry potter', 'anyone', 500, 'not read');
-addBookToLibrary('Sembilu', 'anyone', 500, 'not read');
-addBookToLibrary('Self help', 'anyone', 500, 'not read');
-addBookToLibrary('Great foods', 'anyone', 500, 'not read');
-addBookToLibrary('Coffee vs Tea', 'anyone', 500, 'not read');
-
-library.forEach((book) => {
-    console.log(book.title);
-    const bookCards = document.querySelector('#book-cards');
     const card = document.createElement('div');
     const h1 = document.createElement('h1');
     const div = document.createElement('div');
+    const btn = document.createElement('button');
     card.className = 'card';
-    h1.textContent = book.title;
-    div.textContent = `Author: ${book.author}, Pages: ${book.pages}`;
+    card.setAttribute('data-title-card', addBook.title);
+    h1.textContent = addBook.title;
+    div.textContent = `Author: ${addBook.author}, Pages: ${addBook.pages}`;
+    btn.textContent = 'Remove';
+    btn.setAttribute('data-title-button', addBook.title);
+    btn.addEventListener('click', (e) => {
+        const findtitle = library.findIndex(
+            (item) => item.title === e.target.getAttribute('data-title-button')
+        );
+        library.splice(findtitle, 1);
+        document
+            .querySelector(
+                `[data-title-card="${e.target.getAttribute(
+                    'data-title-button'
+                )}"]`
+            )
+            .remove();
+        console.table(library);
+    });
     bookCards.appendChild(card);
-    card.append(h1, div);
-});
+    card.append(h1, div, btn);
+    console.table(library);
+}
 
-console.table(library);
+addBookToLibrary('The hunger game', 'someone', 200, 'read');
