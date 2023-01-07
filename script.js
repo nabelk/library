90af14ad5bebed3fb6412c87ba52368c233e0224
Nabil Khalid <holanabilkhalid@gmail.com>
2023-01-08 00:19:29 +0800

    Refactor to class

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 const library = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     this.read = read;
+// }
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    toggleRead(status) {
+        this.read = status;
+    }
 }
 
-Book.prototype.toggleRead = function (status) {
-    this.read = status;
-};
+// Book.prototype.toggleRead = function (status) {
+//     this.read = status;
+// };
 
 function addBookToLibrary(title, author, pages, read) {
     const bookCards = document.getElementById('book-cards');
